6d50abc2a5354f3877d5ae832c4f72016d7e4dac
CnupuT <fenikssuper@mail.ru>
2021-07-27 14:47:17 +0300

    убрал try-catch конструкции

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -19,7 +19,7 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
             session.beginTransaction();
             session.createSQLQuery("create table if not exists Users (" +
                     "id bigint auto_increment," +
@@ -29,39 +29,30 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
                     "constraint Users_pk" +
                     " primary key (id))").executeUpdate();
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void dropUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
             session.beginTransaction();
             session.createSQLQuery("drop table if exists Users").executeUpdate();
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
             session.beginTransaction();
             session.save(new User(name, lastName, age));
             session.getTransaction().commit();
             System.out.println("User с именем – "
                     + name
                     + " добавлен в базу данных");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void removeUserById(long id) {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
             session.beginTransaction();
             User user = session.get(User.class, id);
             if (user != null) {
@@ -71,32 +62,23 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
                         + " удалён");
                 session.getTransaction().commit();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> result = new ArrayList<>();
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
             session.beginTransaction();
             result = session.createQuery("from User").list();
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return result;
     }
 
     @Override
     public void cleanUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
             session.beginTransaction();
             session.createQuery("delete from User").executeUpdate();
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
